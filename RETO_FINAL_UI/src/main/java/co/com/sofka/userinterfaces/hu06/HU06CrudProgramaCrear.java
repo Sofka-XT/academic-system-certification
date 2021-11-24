40fd8450d5603ae08f6b0a4946a96e6eca442dcb
DavidUrregoz <73442281+DavidUrregoz@users.noreply.github.com>
2021-11-24 12:29:50 -0500

    Se agregaron los target preplanteados por para el modelo, falta agregar los dentificadores, tambien se agrego la logica de la pruega de edicion

--- RETO_FINAL_UI/src/main/java/co/com/sofka/userinterfaces/hu06/HU06CrudProgramaCrear.java
+++ RETO_FINAL_UI/src/main/java/co/com/sofka/userinterfaces/hu06/HU06CrudProgramaCrear.java
@@ -7,11 +7,5 @@ import static org.openqa.selenium.By.id;
 import static org.openqa.selenium.By.xpath;
 
 public class HU06CrudProgramaCrear extends PageObject {
-    public static final Target USER_NAME = Target
-            .the("NombreUsuario")
-            .located(id("  "));
 
-    public static final Target PROGRAMA = Target
-            .the("Programa")
-            .located(xpath("  "));
 }
