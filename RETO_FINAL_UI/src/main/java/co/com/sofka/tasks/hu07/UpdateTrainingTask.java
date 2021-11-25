7e6125c5473fe1801f5165fefdf94b24d0a23d51
Teo <teo.calle.lara@gmail.com>
2021-11-25 01:53:07 -0500

    Se agregan tasks del crud de training

---
+++ RETO_FINAL_UI/src/main/java/co/com/sofka/tasks/hu07/UpdateTrainingTask.java
@@ -0,0 +1,11 @@
+package co.com.sofka.tasks.hu07;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+public class UpdateTrainingTask implements Task {
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+
+    }
+}
