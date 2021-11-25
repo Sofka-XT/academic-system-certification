a01142363f283be478f522eb7aa100fc579e453d
Harold_git <harold.1408@hotmail.com>
2021-11-25 08:56:38 -0500

    Se modifica clase hu06

--- RETO_FINAL_UI/src/main/java/co/com/sofka/tasks/hu06/create/HU06CrudProgramaCrearTaskNoCursesNoName.java
+++ RETO_FINAL_UI/src/main/java/co/com/sofka/tasks/hu06/create/HU06CrudProgramaCrearTaskNoCursesNoName.java
@@ -3,10 +3,6 @@ package co.com.sofka.tasks.hu06.create;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
-import net.serenitybdd.screenplay.actions.Scroll;
-
-import static co.com.sofka.userinterfaces.hu06.create.HU06CrudProgramaCrear.PROGRAM_NAME;
 import static co.com.sofka.userinterfaces.hu06.create.HU06CrudProgramaCrear.SAVE_PROGRAM;
 
 public class HU06CrudProgramaCrearTaskNoCursesNoName implements Task {
